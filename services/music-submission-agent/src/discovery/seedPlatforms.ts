c3ca163964875ece07fd381ed845ebadc324d375
Marc <marc@adviesnconsultancy.nl>
2026-06-28 23:32:11 +0200

    Wire run 11 music submission seeds into discovery

--- services/music-submission-agent/src/discovery/seedPlatforms.ts
+++ services/music-submission-agent/src/discovery/seedPlatforms.ts
@@ -7,6 +7,7 @@ import { run7SeedPlatforms } from './run7PlatformSeeds.js';
 import { run8SeedPlatforms } from './run8PlatformSeeds.js';
 import { run9SeedPlatforms } from './run9PlatformSeeds.js';
 import { run10SeedPlatforms } from './run10PlatformSeeds.js';
+import { run11SeedPlatforms } from './run11PlatformSeeds.js';
 
 const allSeedPlatforms = [
   ...seedPlatforms,
@@ -15,7 +16,8 @@ const allSeedPlatforms = [
   ...run7SeedPlatforms,
   ...run8SeedPlatforms,
   ...run9SeedPlatforms,
-  ...run10SeedPlatforms
+  ...run10SeedPlatforms,
+  ...run11SeedPlatforms
 ];
 
 export function seedDiscoveryPlatforms(repositories: Repositories): { discovered: number; queued: number } {
