ab145e0a668826deaa475a9212ae5a56dd3bb92f
Marc <marc@adviesnconsultancy.nl>
2026-06-30 21:35:56 +0200

    Include run 30 platform seeds

--- services/music-submission-agent/src/discovery/seedPlatforms.ts
+++ services/music-submission-agent/src/discovery/seedPlatforms.ts
@@ -27,6 +27,7 @@ import { run26SeedPlatforms } from './run26PlatformSeeds.js';
 import { run27SeedPlatforms } from './run27PlatformSeeds.js';
 import { run28SeedPlatforms } from './run28PlatformSeeds.js';
 import { run29SeedPlatforms } from './run29PlatformSeeds.js';
+import { run30SeedPlatforms } from './run30PlatformSeeds.js';
 
 const allSeedPlatforms = [
   ...seedPlatforms,
@@ -54,7 +55,8 @@ const allSeedPlatforms = [
   ...run26SeedPlatforms,
   ...run27SeedPlatforms,
   ...run28SeedPlatforms,
-  ...run29SeedPlatforms
+  ...run29SeedPlatforms,
+  ...run30SeedPlatforms
 ];
 
 export function seedDiscoveryPlatforms(repositories: Repositories): { discovered: number; queued: number } {
