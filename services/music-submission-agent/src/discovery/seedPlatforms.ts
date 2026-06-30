37cea4c7009acfcb3480372f33c0da54125e4654
Marc <marc@adviesnconsultancy.nl>
2026-06-30 14:30:30 +0200

    Wire run 23 seeds into discovery pipeline

--- services/music-submission-agent/src/discovery/seedPlatforms.ts
+++ services/music-submission-agent/src/discovery/seedPlatforms.ts
@@ -20,6 +20,7 @@ import { run19SeedPlatforms } from './run19PlatformSeeds.js';
 import { run20SeedPlatforms } from './run20PlatformSeeds.js';
 import { run21SeedPlatforms } from './run21PlatformSeeds.js';
 import { run22SeedPlatforms } from './run22PlatformSeeds.js';
+import { run23SeedPlatforms } from './run23PlatformSeeds.js';
 
 const allSeedPlatforms = [
   ...seedPlatforms,
@@ -40,7 +41,8 @@ const allSeedPlatforms = [
   ...run19SeedPlatforms,
   ...run20SeedPlatforms,
   ...run21SeedPlatforms,
-  ...run22SeedPlatforms
+  ...run22SeedPlatforms,
+  ...run23SeedPlatforms
 ];
 
 export function seedDiscoveryPlatforms(repositories: Repositories): { discovered: number; queued: number } {
