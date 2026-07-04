961176ff06c27d620aa0d3edfc08673bbb7366a7
Marc <marc@adviesnconsultancy.nl>
2026-07-04 06:34:00 +0200

    Load run 109 seed platforms

--- services/music-submission-agent/src/discovery/seedPlatforms.ts
+++ services/music-submission-agent/src/discovery/seedPlatforms.ts
@@ -96,6 +96,7 @@ import { run105SeedPlatforms } from './run105PlatformSeeds.js';
 import { run106SeedPlatforms } from './run106PlatformSeeds.js';
 import { run107SeedPlatforms } from './run107PlatformSeeds.js';
 import { run108SeedPlatforms } from './run108PlatformSeeds.js';
+import { run109SeedPlatforms } from './run109PlatformSeeds.js';
 
 const allSeedPlatforms = [
   ...seedPlatforms,
@@ -192,7 +193,8 @@ const allSeedPlatforms = [
   ...run105SeedPlatforms,
   ...run106SeedPlatforms,
   ...run107SeedPlatforms,
-  ...run108SeedPlatforms
+  ...run108SeedPlatforms,
+  ...run109SeedPlatforms
 ];
 
 export function seedDiscoveryPlatforms(repositories: Repositories): { discovered: number; queued: number } {
